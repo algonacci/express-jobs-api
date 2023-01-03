46d0b0c5b1c0f74dde2f8674ba77849f2b7824d2
Eric Julianto <algonacci1@gmail.com>
2023-01-03 15:15:48 +0700

    updates

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`🚀 [SERVER] is running on port http://localhost:${port}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = (url) => {
+  console.log("💽 [DATABASE] MongoDB Connection Ready!");
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-}
+  });
+};
 
-module.exports = connectDB
+module.exports = connectDB;
